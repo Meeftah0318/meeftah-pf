44e3be2be9a32a8b490f28cb055cf3e839520bdc
Meeftah0318 <47889367+Meeftah0318@users.noreply.github.com>
2022-07-30 23:04:54 +0300

    first commit

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="mx-auto">
+      <h1>Welcome to React Router!</h1>
+      <h1>This is Navbar</h1>
+    </div>
+  );
+};
+
+export default Home;
